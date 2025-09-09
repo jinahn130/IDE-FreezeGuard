1c32fe54181cd59a84486b587a3c8d18c7b37d99
Jin Seong Ahn <jinseongahn@Mac.home>
2025-09-08 17:13:49 -0700

    build error due to date

--- intellij-freezeguard/build.gradle.kts
+++ intellij-freezeguard/build.gradle.kts
@@ -59,7 +59,7 @@ intellijPlatform {
 changelog {
     version.set("1.0.0")
     path.set("${project.projectDir}/CHANGELOG.md")
-    header.set(provider { "[${version.get()}] - ${date()}" })
+    header.set(provider { "[${version.get()}] - ${java.time.LocalDate.now()}" })
     headerParserRegex.set("""(\d+\.\d+\.\d+)""".toRegex())
     introduction.set("""
         IDE FreezeGuard is a cross-platform performance monitoring solution for IntelliJ IDEA and Visual Studio Code.
